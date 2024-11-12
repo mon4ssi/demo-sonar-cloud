2b0fdcaeeb9ce864aaa7f121244fa1a6ff362f31
Monassi Carrasco <mon4ssi@outlook.com>
2024-11-12 15:33:01 -0500

    ci: update bash

--- DemoSonar/run_sonnar.bat
+++ DemoSonar/run_sonnar.bat
@@ -1,3 +1,5 @@
-dotnet sonarscanner begin /o:"usuariotmp001" /k:"usuariotmp001_demoSonar" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.token="ca62dcb0d053760142e0b51651313e66729f8716" /d:sonar.scanner.scanAll=false
+dotnet sonarscanner begin /o:"mon4ssi" /k:"mon4ssi_demo-sonar-cloud" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.token="2a4e04299097c71e2f0f00097f462a0566e8990a"
+
 dotnet build
-dotnet sonarscanner end /d:sonar.token="ca62dcb0d053760142e0b51651313e66729f8716"
+
+dotnet sonarscanner end /d:sonar.token="2a4e04299097c71e2f0f00097f462a0566e8990a"
