67c6521b3a78f190414f060c1d2694c5ce12ac16
Monassi Carrasco <mon4ssi@outlook.com>
2024-11-12 15:28:20 -0500

    ci: add bash for unix systems

---
+++ DemoSonar/sonnar.sh
@@ -0,0 +1,9 @@
+dotnet sonarscanner begin \
+  /o:"mon4ssi" \
+  /k:"mon4ssi_demo-sonar-cloud" \
+  /d:sonar.host.url="https://sonarcloud.io" \
+  /d:sonar.token="2a4e04299097c71e2f0f00097f462a0566e8990a"
+
+dotnet build
+
+dotnet sonarscanner end /d:sonar.token="2a4e04299097c71e2f0f00097f462a0566e8990a"
